cf957e45881621627d282f665f93fd5ac2fce7cf
Paolo Patierno <ppatierno@live.com>
2022-07-17 15:28:53 +0200

    Added skeleton projects for producer and consumer
    Signed-off-by: Paolo Patierno <ppatierno@live.com>

---
+++ kafka-consumer/src/main/java/io/ppatierno/kafka/opentelemetry/Consumer.java
@@ -0,0 +1,8 @@
+package io.ppatierno.kafka.opentelemetry;
+
+public class Consumer {
+
+    public static void main(String[] args) {
+        // TBD
+    }
+}
---
+++ kafka-producer/src/main/java/io/ppatierno/kafka/opentelemetry/Producer.java
@@ -0,0 +1,8 @@
+package io.ppatierno.kafka.opentelemetry;
+
+public class Producer {
+
+    public static void main(String[] args) {
+        // TBD
+    }
+}
