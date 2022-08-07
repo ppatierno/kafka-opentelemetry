7e8f8928a4e5c97475b86d9a0668157933a1e9d2
Paolo Patierno <ppatierno@live.com>
2022-08-07 10:38:24 +0200

    Added Kafka Streams using interceptor and manual SDK configuration
    Signed-off-by: Paolo Patierno <ppatierno@live.com>

---
+++ kafka-streams/src/main/java/io/ppatierno/kafka/opentelemetry/Streams.java
@@ -0,0 +1,67 @@
+package io.ppatierno.kafka.opentelemetry;
+
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.api.common.Attributes;
+import io.opentelemetry.api.trace.propagation.W3CTraceContextPropagator;
+import io.opentelemetry.context.propagation.ContextPropagators;
+import io.opentelemetry.exporter.jaeger.JaegerGrpcSpanExporter;
+import io.opentelemetry.instrumentation.kafkaclients.TracingConsumerInterceptor;
+import io.opentelemetry.instrumentation.kafkaclients.TracingProducerInterceptor;
+import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.sdk.resources.Resource;
+import io.opentelemetry.sdk.trace.SdkTracerProvider;
+import io.opentelemetry.sdk.trace.export.BatchSpanProcessor;
+import io.opentelemetry.sdk.trace.samplers.Sampler;
+import io.opentelemetry.semconv.resource.attributes.ResourceAttributes;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.streams.StreamsConfig;
+
+import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
+
+public class Streams extends BaseStreams {
+
+    public static void main(String[] args) {
+        configureOpenTelemetry();
+
+        Streams streams = new Streams();
+        streams.loadConfiguration(System.getenv());
+        Properties props = streams.loadKafkaStreamsProperties();
+        props.put(StreamsConfig.CONSUMER_PREFIX + ConsumerConfig.INTERCEPTOR_CLASSES_CONFIG, TracingConsumerInterceptor.class.getName());
+        props.put(StreamsConfig.PRODUCER_PREFIX + ProducerConfig.INTERCEPTOR_CLASSES_CONFIG, TracingProducerInterceptor.class.getName());
+
+        CountDownLatch latch = new CountDownLatch(1);
+
+        Runtime.getRuntime().addShutdownHook(new Thread("streams-shutdown-hook") {
+            @Override
+            public void run() {
+                streams.stop();
+                latch.countDown();
+            }
+        });
+
+        try {
+            streams.run(props);
+            latch.await();
+        } catch (Throwable e) {
+            System.exit(1);
+        }
+    }
+
+    private static void configureOpenTelemetry() {
+        Resource resource = Resource.getDefault()
+                .merge(Resource.create(Attributes.of(ResourceAttributes.SERVICE_NAME, "my-kafka-service")));
+
+        SdkTracerProvider sdkTracerProvider = SdkTracerProvider.builder()
+                .addSpanProcessor(BatchSpanProcessor.builder(JaegerGrpcSpanExporter.builder().build()).build())
+                .setSampler(Sampler.alwaysOn())
+                .setResource(resource)
+                .build();
+
+        OpenTelemetry openTelemetry = OpenTelemetrySdk.builder()
+                .setTracerProvider(sdkTracerProvider)
+                .setPropagators(ContextPropagators.create(W3CTraceContextPropagator.getInstance()))
+                .buildAndRegisterGlobal();
+    }
+}
